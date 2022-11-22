d295bf494184ee1c3674a48d09cf30ddd98fa3b7
Ritik Srivastava <96912105+ritik-sri@users.noreply.github.com>
2022-11-23 00:12:37 +0530

    Added solution - LeetHub

---
+++ Occurence of an integer in a Linked List - GFG/occurence-of-an-integer-in-a-linked-list.py
@@ -0,0 +1,60 @@
+"""  
+class Node:
+    def __init__(self, data):
+		self.data = data
+		self.next = None
+  This is method only submission.
+  You only need to complete the method.
+"""
+class Solution:
+    def count(self, head, search_for):
+        # Code here
+        count=0
+        while head:
+            if head.data==search_for:
+                count+=1
+            head=head.next
+        return count
+
+
+#{ 
+ # Driver Code Starts
+# Do not delete this comment
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+
+
+class LinkedList:
+    def __init__(self):
+        self.head = None
+
+    def push(self, new_data):
+        new_node = Node(new_data)
+        new_node.next = self.head
+        self.head = new_node
+
+    def printList(self):
+        temp = self.head
+        while (temp):
+            print(temp.data, end=" ")
+            # arr.append(str(temp.data))
+            temp = temp.next
+        print()
+
+
+if __name__ == '__main__':
+    t = int(input())
+    while (t > 0):
+        llist = LinkedList()
+        n = int(input())
+        values = list(map(int, input().split()))
+        for i in reversed(values):
+            llist.push(i)
+        m = int(input())
+        k = Solution().count(llist.head, m)
+        print(k)
+        t -= 1
+# Contributed by:Harshit Sidhwa
+# } Driver Code Ends
