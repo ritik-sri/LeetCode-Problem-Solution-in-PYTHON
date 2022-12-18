19eb54acab03ce22c25c36f3f848e5fee21a27d9
Ritik Srivastava <96912105+ritik-sri@users.noreply.github.com>
2022-12-18 10:49:28 +0530

    Time: 143 ms (78.57%), Space: 13.9 MB (64.29%) - LeetHub

---
+++ 2506-count-pairs-of-similar-strings/2506-count-pairs-of-similar-strings.py
@@ -0,0 +1,11 @@
+class Solution:
+    def similarPairs(self, words: List[str]) -> int:
+
+        d=Counter()
+        count=0
+        for current_word in words:
+            word="".join(sorted(set(current_word)))
+            count+=d[word]
+            d[word]+=1
+
+        return count    
