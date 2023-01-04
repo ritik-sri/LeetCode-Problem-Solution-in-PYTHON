7978b3bb6cfe735f9885a93f98a9ecf7eba22e32
Ritik Srivastava <96912105+ritik-sri@users.noreply.github.com>
2023-01-04 14:11:54 +0530

    Time: 1005 ms (82.13%), Space: 28.2 MB (70.66%) - LeetHub

--- 2244-minimum-rounds-to-complete-all-tasks/2244-minimum-rounds-to-complete-all-tasks.py
+++ 2244-minimum-rounds-to-complete-all-tasks/2244-minimum-rounds-to-complete-all-tasks.py
@@ -1,15 +1,5 @@
 from collections import Counter
 class Solution:
-    def minimumRounds(self, tasks: List[int]) -> int:
-        hash=Counter(tasks)
-        a=hash.values()
-        count=0
-        for i in a:
-            if i<2:
-                return -1
-            if i>3:
-                g=math.ceil(i/3)
-                count+=g
-            else:
-                count+=1
-        return count
+      def minimumRounds(self, tasks):
+        freq = Counter(tasks).values()
+        return -1 if 1 in freq else sum((a + 2) // 3 for a in freq)
