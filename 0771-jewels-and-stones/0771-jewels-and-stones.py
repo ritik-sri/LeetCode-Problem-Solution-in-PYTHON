db1399847dd74f26af0de292f743a024932bddfe
Ritik Srivastava <96912105+ritik-sri@users.noreply.github.com>
2023-02-27 13:47:34 +0530

    Time: 25 ms (95.28%), Space: 13.8 MB (95.29%) - LeetHub

--- 0771-jewels-and-stones/0771-jewels-and-stones.py
+++ 0771-jewels-and-stones/0771-jewels-and-stones.py
@@ -1,8 +1,9 @@
 class Solution:
     def numJewelsInStones(self, jewels: str, stones: str) -> int:
         count=0
+        jewels=set(jewels)
         for i in stones:
-            if i in jewels[:]:
+            if i in jewels:
                 count+=1
         return count
     
