5529bf835ebffcd54ba36255f167bb134566d99f
Ritik Srivastava <96912105+ritik-sri@users.noreply.github.com>
2023-03-01 20:50:11 +0530

    Time: 561 ms (29.56%), Space: 47.5 MB (18.87%) - LeetHub

--- 1609-even-odd-tree/1609-even-odd-tree.py
+++ 1609-even-odd-tree/1609-even-odd-tree.py
@@ -1,35 +1,62 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
 class Solution:
     def isEvenOddTree(self, root: Optional[TreeNode]) -> bool:
+        bfs=[]
         if root is None:
-            return False
-        
-        q = deque([])
+            return bfs
+        q=deque([])
         q.append(root)
-        c = 0
-        
         while q:
-            lvl_size = len(q)
-            prev = None
-            
+            lvl_size=len(q)
+            curr_lvl=[]
             for i in range(lvl_size):
-                curr = q.popleft()
-                
-                # check if the current node value satisfies the Even-Odd conditions
-                if c % 2 == 0:
-                    if curr.val % 2 != 1 or (prev is not None and curr.val <= prev):
-                        return False
-                else:
-                    if curr.val % 2 != 0 or (prev is not None and curr.val >= prev):
-                        return False
-                
-                prev = curr.val
-                
-                # add the child nodes to the queue
+                curr=q.popleft()
+                curr_lvl.append(curr.val)
                 if curr.left is not None:
                     q.append(curr.left)
                 if curr.right is not None:
                     q.append(curr.right)
-            
-            c += 1
-        
-        return True
+            bfs.append(curr_lvl)
+        print(bfs)
+        c=1
+        flag=0
+        if bfs[0][0] %2!=0:
+            flag=1
+        for arr in bfs[1:]:
+            if c%2==0:
+                if len(arr)==1:
+                    if(arr[0]%2==0):
+                        return False
+                if len(arr)==2:
+                    if(arr[-1]%2==0):
+                        return False
+                if len(arr)==3:
+                    if arr[-1]%2==0:
+                        return False
+                for j in range(len(arr)-1):
+                    if(arr[j]%2!=0 and arr[j]<arr[j+1]):
+                        continue
+                    else:
+                        return False
+            else:
+                if len(arr)==1:
+                    if(arr[0]%2!=0):
+                        return False
+                if len(arr)==2:
+                    if(arr[-1]%2!=0):
+                        return False
+                for j in range(len(arr)-1):
+                    if(arr[j]%2==0 and arr[j]>arr[j+1]):
+                        continue
+                    else:
+                        return False
+            c+=1
+        if flag==1:
+            return True
+        else:
+            return False
