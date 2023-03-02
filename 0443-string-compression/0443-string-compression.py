bbefab82e28cb18d03c5c705384f5c995347a607
Ritik Srivastava <96912105+ritik-sri@users.noreply.github.com>
2023-03-03 00:05:49 +0530

    Time: 68 ms (28.47%), Space: 14 MB (25.81%) - LeetHub

--- 0443-string-compression/0443-string-compression.py
+++ 0443-string-compression/0443-string-compression.py
@@ -15,4 +15,22 @@ class Solution:
                         chars[i] = digit
                         i += 1
                 count = 1
+        return len(chars)
+from typing import List
+
+class Solution:
+    def compress(self, chars: List[str]) -> int:
+        count = 1
+        i = 0
+        for j in range(1, len(chars)+1):
+            if j < len(chars) and chars[j] == chars[j-1]:
+                count += 1
+            else:
+                chars[i] = chars[j-1]
+                i += 1
+                if count > 1:
+                    for digit in str(count):
+                        chars[i] = digit
+                        i += 1
+                count = 1
         return i
