27dc6ff9db8d32a491e901f8b82e73941f853c6e
Ritik Srivastava <96912105+ritik-sri@users.noreply.github.com>
2023-03-04 15:52:20 +0530

    Time: 51 ms (67.48%), Space: 13.8 MB (70.35%) - LeetHub

---
+++ 0012-integer-to-roman/0012-integer-to-roman.py
@@ -0,0 +1,17 @@
+class Solution:
+    def intToRoman(self, num: int) -> str:
+        map = {
+            1: "I",
+            5: "V",    4: "IV",
+            10: "X",   9: "IX",
+            50: "L",   40: "XL",
+            100: "C",  90: "XC",
+            500: "D",  400: "CD",
+            1000: "M", 900: "CM",
+        }
+        res = ''
+        for n in [1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1]:
+            while n <= num:
+                res += map[n]
+                num-=n
+        return res
