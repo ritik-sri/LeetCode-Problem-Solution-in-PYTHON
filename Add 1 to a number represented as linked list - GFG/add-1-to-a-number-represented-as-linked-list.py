41d2c5b6783f892d66b9ec0e6916f470aa662173
Ritik Srivastava <96912105+ritik-sri@users.noreply.github.com>
2023-03-06 23:25:46 +0530

    Added solution - LeetHub

---
+++ Add 1 to a number represented as linked list - GFG/add-1-to-a-number-represented-as-linked-list.py
@@ -0,0 +1,73 @@
+#User function Template for python3
+
+'''
+
+class Node:
+    def __init__(self, data):   # data -> value stored in node
+        self.data = data
+        self.next = None
+'''
+
+class Solution:
+    def addOne(self,head):
+        #Returns new head of linked List.
+        a=''
+        while head:
+            a+=str(head.data)
+            head=head.next
+        a=int(a)
+        a=str(a+1)
+        a=list(a)
+        dummy=Node(-1)
+        p=dummy
+        i=0
+        while i<len(a):
+            dummy.next=Node(a[i])
+            dummy=dummy.next
+            i+=1
+        return p.next
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+
+# Node Class
+class Node:
+    def __init__(self, data):   # data -> value stored in node
+        self.data = data
+        self.next = None
+
+# Linked List Class
+class LinkedList:
+    def __init__(self):
+        self.head = None
+        self.tail = None
+
+    # creates a new node with given value and appends it at the end of the linked list
+    def insert(self, value):
+        if self.head is None:
+            self.head = Node(value)
+            self.tail = self.head
+        else:
+            self.tail.next = Node(value)
+            self.tail = self.tail.next
+
+def PrintList(head):
+    while head:
+        print(head.data,end='')
+        head = head.next
+
+if __name__ == '__main__':
+    t=int(input())
+    for _ in range(t):
+        
+        num = input()
+        ll = LinkedList() # create a new linked list 'll1'.
+        for digit in num:
+            ll.insert(int(digit))  # add to the end of the list
+        
+        resHead = Solution().addOne(ll.head)
+        PrintList(resHead)
+        print()
+
+
+# } Driver Code Ends
