ef4491a0db08a50d7c4b913968c946e357e6ce09
Ritik Srivastava <96912105+ritik-sri@users.noreply.github.com>
2023-03-06 23:04:17 +0530

    Added solution - LeetHub

--- Geek hates too many 1s - GFG/geek-hates-too-many-1s.py
+++ Geek hates too many 1s - GFG/geek-hates-too-many-1s.py
@@ -1,16 +1,14 @@
 class Solution:
     def noConseBits(self, n: int) -> int:
-        a = bin(n)
-        i = 2
+        a = bin(n)[2:]
         b = a
+        i=0
         while i <= len(a):
             if a[i:i+3] == '111':
                 b = a[:i+2] + '0' + a[i+3:]
                 a = b
             i += 1
         return int(a, 2)
-
-
 #{ 
  # Driver Code Starts
 if __name__=="__main__":
