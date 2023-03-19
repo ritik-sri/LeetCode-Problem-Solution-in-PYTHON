76f64f633881ef92c7717a164e8de863e3a69390
Ritik Srivastava <96912105+ritik-sri@users.noreply.github.com>
2023-03-19 23:20:26 +0530

    Added solution - LeetHub

--- Distinct Difference - GFG/distinct-difference.py
+++ Distinct Difference - GFG/distinct-difference.py
@@ -2,16 +2,15 @@ from typing import List
 class Solution:
     def getDistinctDifference(self, N : int, A : List[int]) -> List[int]:
         s1=set()
-        s2=set()
         left=[]
         right=[]
         for i in range(N):
             left.append(len(s1))
             s1.add(A[i])
-            
+        s1=set()
         for j in range(len(A)-1,-1,-1):
-            right.append(len(s2))
-            s2.add(A[j])
+            right.append(len(s1))
+            s1.add(A[j])
         right=right[::-1]
         res=[]
         for i in range(N):
