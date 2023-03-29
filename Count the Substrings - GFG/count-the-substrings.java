b9e05dc7a07144084f8259976098c2d5f1c707eb
Ritik Srivastava <96912105+ritik-sri@users.noreply.github.com>
2023-03-29 22:40:42 +0530

    Added solution - LeetHub

--- Count the Substrings - GFG/count-the-substrings.java
+++ Count the Substrings - GFG/count-the-substrings.java
@@ -16,33 +16,24 @@ class GFG{
     } 
 } 
 // } Driver Code Ends
-
-
 //User function Template for Java
 class Solution 
 { 
     public static int countSubstring(String S) {
     int count = 0;
-    // Iterate over each character of the string S
     for (int i = 0; i < S.length(); i++) {
-        // Initialize variables to keep track of the count of uppercase and lowercase letters
         int upperCount = 0, lowerCount = 0;
-        // Iterate over each substring starting from the current index i
         for (int j = i; j < S.length(); j++) {
-            // Update the count of uppercase and lowercase letters based on the current character
             if (Character.isUpperCase(S.charAt(j))) {
                 upperCount++;
             } else {
                 lowerCount++;
             }
-            // If the count of uppercase and lowercase letters is equal, then increment the count variable
             if (upperCount == lowerCount) {
                 count++;
             }
         }
     }
-    // Return the count variable, which represents the total number of substrings that have an equal
-    // number of uppercase and lowercase letters
     return count;
 }
 } 
