89b1114491d46dbd45590f100ddf57d0cf0e5ec8
Ritik Srivastava <96912105+ritik-sri@users.noreply.github.com>
2023-04-01 21:05:22 +0530

    Added solution - LeetHub

---
+++ Find the Highest number - GFG/find-the-highest-number.py
@@ -0,0 +1,28 @@
+#User function Template for python3
+
+class Solution:
+	def findPeakElement(self, a):
+		# Code here
+		return max(a)
+		
+		
+
+
+
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+
+if __name__ == '__main__':
+	T=int(input())
+	for i in range(T):
+		n = int(input())
+		a = list(map(int,input().split()))
+		ob = Solution();
+		ans = ob.findPeakElement(a)
+		print(ans)
+
+
+
+
+# } Driver Code Ends
