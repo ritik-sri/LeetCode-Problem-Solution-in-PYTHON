708920b861f7a31bae7c3431cb3a196353c56419
Ritik Srivastava <96912105+ritik-sri@users.noreply.github.com>
2023-04-15 19:45:15 +0530

    Added solution - LeetHub

---
+++ Find Total Time Taken - GFG/find-total-time-taken.py
@@ -0,0 +1,52 @@
+from collections import defaultdict
+from typing import List
+class Solution:
+    def totalTime(self, n : int, arr : List[int], time : List[int]) -> int:
+        # code here
+        ans=0
+        dic=defaultdict(int)
+        for i in range(0,n):
+            if arr[i] not in dic:
+                ans+=1
+                dic[arr[i]]+=1
+            else:
+                ans+=time[arr[i]-1]
+                dic[arr[i]]+=1
+        return ans-1
+        
+
+
+
+#{ 
+ # Driver Code Starts
+class IntArray:
+    def __init__(self) -> None:
+        pass
+    def Input(self,n):
+        arr=[int(i) for i in input().strip().split()]#array input
+        return arr
+    def Print(self,arr):
+        for i in arr:
+            print(i,end=" ")
+        print()
+
+
+if __name__=="__main__":
+    t = int(input())
+    for _ in range(t):
+        
+        n = int(input())
+        
+        
+        arr=IntArray().Input(n)
+        
+        
+        time=IntArray().Input(n)
+        
+        obj = Solution()
+        res = obj.totalTime(n, arr, time)
+        
+        print(res)
+        
+
+# } Driver Code Ends
