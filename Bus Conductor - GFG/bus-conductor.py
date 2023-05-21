ef29f23e04a6af902e3b2eeddac5a8067986f75b
Ritik Srivastava <96912105+ritik-sri@users.noreply.github.com>
2023-05-21 09:26:33 +0530

    Added solution - LeetHub

---
+++ Bus Conductor - GFG/bus-conductor.py
@@ -0,0 +1,35 @@
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+
+# } Driver Code Ends
+#User function Template for python3
+
+class Solution:
+    def findMoves(self,n,chairs,passengers):
+        #code here
+        sumi=0
+        chairs.sort()
+        passengers.sort()
+        for i,j in zip(chairs,passengers):
+            sumi+=abs(i-j)
+        return sumi
+        
+        
+        
+        
+        
+        
+        
+
+#{ 
+ # Driver Code Starts.
+if __name__=="__main__":
+    for _ in range(int(input())):
+        n=int(input())
+        a=[int(i) for i in input().split()]
+        b=[int(i) for i in input().split()]
+        obj=Solution()
+        print(obj.findMoves(n,a,b))
+        
+# } Driver Code Ends
