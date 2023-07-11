e769a2574c41e7defd9a12d09e913b07a3b65a7f
Ritik Srivastava <96912105+ritik-sri@users.noreply.github.com>
2023-07-11 14:16:41 +0530

    Added solution - LeetHub

---
+++ Find kth element of spiral matrix - GFG/find-kth-element-of-spiral-matrix.cpp
@@ -0,0 +1,90 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+#define MAX 1000
+
+// } Driver Code Ends
+class Solution
+{
+    public:
+    /*You are required to complete this method*/
+    int findK(int a[MAX][MAX],int n,int m,int k)
+    {
+         // Your code goes here.
+         vector<int> ans;
+ 
+  // Initialize the pointers reqd for traversal.
+  int top = 0, left = 0, bottom = n - 1, right = m - 1;
+
+  // Loop until all elements are not traversed.
+  while (top <= bottom && left <= right) {
+      
+    // For moving left to right
+    for (int i = left; i <= right; i++)
+      ans.push_back(a[top][i]);
+
+    top++;
+
+    // For moving top to bottom.
+    for (int i = top; i <= bottom; i++)
+      ans.push_back(a[i][right]);
+
+    right--;
+    
+    // For moving right to left.
+    if (top <= bottom) {
+      for (int i = right; i >= left; i--)
+       ans.push_back(a[bottom][i]);
+
+      bottom--;
+    }
+
+    // For moving bottom to top.
+    if (left <= right) {
+      for (int i = bottom; i >= top; i--)
+        ans.push_back(a[i][left]);
+
+      left++;
+    }
+  }
+
+//kth element will be present at (k-1)th index in ans vector
+  return ans[k-1];
+  
+    }
+
+
+};
+
+
+
+
+
+//{ Driver Code Starts.
+int main()
+{
+    int T;
+    cin>>T;
+  
+    while(T--)
+    {
+        int n,m;
+        int k=0;
+        //cin>>k;
+        cin>>n>>m>>k;
+        int a[MAX][MAX];
+        
+        for(int i=0;i<n;i++)
+        {
+            for(int j=0;j<m;j++)
+            {
+                cin>>a[i][j];
+            }
+        }
+        Solution ob;
+        cout<<ob.findK(a,n,m,k)<<endl;
+        
+       
+    }
+}
+// } Driver Code Ends
