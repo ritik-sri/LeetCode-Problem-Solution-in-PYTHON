2ab88c5817b76ed7cfbe8ab3c2f7c0037e22e0de
Ritik Srivastava <96912105+ritik-sri@users.noreply.github.com>
2023-10-20 23:07:04 +0530

    Added solution - LeetHub

---
+++ Form a number divisible by 3 using array digits - GFG/form-a-number-divisible-by-3-using-array-digits.py
@@ -0,0 +1,25 @@
+#User function Template for python3
+
+class Solution:
+    def isPossible(self, N, arr):
+        # code here
+        if sum(arr)% 3 == 0:
+            return 1
+        return 0
+
+
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+
+if __name__ == '__main__':
+    t = int(input())
+    for _ in range(t):
+        N = int(input())
+        arr = input().split()
+        for i in range(N):
+            arr[i] = int(arr[i])
+        
+        ob = Solution()
+        print(ob.isPossible(N, arr))
+# } Driver Code Ends
