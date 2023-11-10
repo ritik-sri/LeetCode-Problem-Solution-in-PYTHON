b3e1f542e5fc73ec30022a0be2c5308e6ce793e5
Ritik Srivastava <96912105+ritik-sri@users.noreply.github.com>
2023-11-10 11:06:43 +0530

    Time: 1110 ms (9.48%), Space: 171 MB (22.18%) - LeetHub

---
+++ 1743-restore-the-array-from-adjacent-pairs/1743-restore-the-array-from-adjacent-pairs.py
@@ -0,0 +1,26 @@
+class Solution:
+    def restoreArray(self, adjacentPairs: List[List[int]]) -> List[int]:
+		# create the map 
+        adj = collections.defaultdict(list)
+        for a, b in adjacentPairs:
+            adj[a].append(b)
+            adj[b].append(a)
+
+		# find the start num
+        start = adjacentPairs[0][0]
+        for k, v in adj.items():
+            if len(v) ==1:
+                start = k
+                break
+				
+		# dfs to connect the graph
+        nums=[]
+        seen = set()
+        def dfs(num):
+            nums.append(num)
+            seen.add(num)
+            for next_num in adj[num]:
+                if next_num in seen: continue
+                dfs(next_num)
+        dfs(start)
+        return nums
