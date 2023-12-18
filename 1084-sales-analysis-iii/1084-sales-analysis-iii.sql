e7cfd6c42d9d2c1a3d7b18cc0bbf9151d638e70b
Ritik Srivastava <96912105+ritik-sri@users.noreply.github.com>
2023-12-18 14:42:03 +0530

    Time: 1817 ms (61.40%), Space: 0B (100.00%) - LeetHub

--- 1084-sales-analysis-iii/1084-sales-analysis-iii.sql
+++ 1084-sales-analysis-iii/1084-sales-analysis-iii.sql
@@ -1,6 +1,6 @@
 select p.product_id as product_id, p.product_name as product_name
 from product p
-right join sales s
+join sales s
 on p.product_id = s.product_id
 group by p.product_id
 having min(s.sale_date) >= '2019-01-01' and max(s.sale_date) <= '2019-03-31';
