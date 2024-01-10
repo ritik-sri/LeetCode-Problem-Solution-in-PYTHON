a77d6f3f57414d810bde71bdb207bcebc424b593
Ritik Srivastava <96912105+ritik-sri@users.noreply.github.com>
2024-01-10 09:38:24 +0530

    Time: 344 ms (96.46%), Space: 55.4 MB (96.28%) - LeetHub

---
+++ 2385-amount-of-time-for-binary-tree-to-be-infected/2385-amount-of-time-for-binary-tree-to-be-infected.py
@@ -0,0 +1,40 @@
+from collections import defaultdict, deque
+from typing import Optional
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+
+class Solution:
+    def amountOfTime(self, root: Optional[TreeNode], start: int) -> int:
+        def adjacentList(root):
+            graph = defaultdict(list)
+            q = deque([root])
+            while q:
+                node = q.popleft()
+                if node.left:
+                    graph[node.val].append(node.left.val)
+                    graph[node.left.val].append(node.val)
+                    q.append(node.left)
+                if node.right:
+                    graph[node.val].append(node.right.val)
+                    graph[node.right.val].append(node.val)
+                    q.append(node.right)
+            return graph
+        adj=adjacentList(root)
+        
+        q = deque([start])  
+        count = -1
+        visited ={start}
+        while q:
+            for _ in range(len(q)):
+                a = q.popleft()
+                for i in adj[a]:
+                    if i not in visited:
+                        q.append(i)
+                        visited.add(i)
+            count += 1
+        return count
